c92469c1478b9ae15bcc3e388c9c245e8744009c
badroum <julien.dufee@gmail.com>
2020-11-24 22:27:17 +0100

    Update Elastic_Search.py

--- Elastic_Search.py
+++ Elastic_Search.py
@@ -4,13 +4,17 @@ class ElasticSearch:
     def __init__(self, index):
         es_host     = os.environ['es_hostname']
         es_port     = os.environ['es_port']
+        es_username     = os.environ['es_username']
+        es_password     = os.environ['es_password']
+
 
         if es_host is None:
             exit('You need to export Elasticsearch hostname')
         if es_port is None:
             exit('You need to export Elasticsearch port number')
-        
-        self.es     = Elasticsearch([{'host': es_host, 'port': es_port}])
+
+        self.es     = Elasticsearch([{'host': es_host, 'port': es_port}], http_auth=(es_username, es_password))
+
         self.index  = index
 
 
